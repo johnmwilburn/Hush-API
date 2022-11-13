ba6920ab08360a8e284b2863af83e35a9d310f6d
John Wilburn <johnwilburn2@gmail.com>
2022-11-12 22:06:28 -0600

    arrow functions

--- App.js
+++ App.js
@@ -22,97 +22,14 @@ async function main() {
   );
 }
 
-app.get("/schedule/get-current", (req, res) => {
-  const schedules = Schedule.find({});
-  const currentSchedule = schedules[schedules.length - 1];
-
-  return res.send(currentSchedule);
-});
-
-app.post("/schedule/change-status", (req, res) => {
-  const reqBody = { command: "hush" };
-
-  if (!reqBody?.command) {
-    return res.status(400);
-  }
-
-  const schedules = Schedule.find({});
-  const currentSchedule = schedules[schedules.length - 1];
-
-  switch (reqBody.command) {
-    case "hush":
-      currentSchedule.totalHush = true;
-      currentSchedule.totalFree = false;
-      break;
-    case "free":
-      currentSchedule.totalHush = false;
-      currentSchedule.totalFree = true;
-      break;
-    case "none":
-    default:
-      currentSchedule.totalHush = false;
-      currentSchedule.totalFree = false;
-      break;
-  }
-
-  currentSchedule.save();
-
-  return res.send(currentSchedule);
-});
-
-app.post("/event/change-status", (req, res) => {
-  const reqBody = { eventUUID: "1", status: "busy" };
-
-  if (!(reqBody?.eventUUID && reqBody?.status)) {
-    return res.status(400);
-  }
-
-  const schedules = Schedule.find({});
-  const currentSchedule = schedules[schedules.length - 1];
-  for (i = 0; i < currentSchedule.events.length; i++) {
-    if (currentSchedule.events[i].eventUUID == reqBody.eventUUID) {
-      currentSchedule.events[i].status = reqBody.status;
-      break;
-    }
-  }
-  currentSchedule.save();
-  return res.send("Received a POST HTTP method");
-});
-
-function changeEventStatus() {
-  const reqBody = { eventUUID: "1", status: "busy" };
-
-  if (!(reqBody?.eventUUID && reqBody?.status)) {
-    return; // res.status(400);
-  }
-
-  const schedules = Schedule.find({});
-  const currentSchedule = schedules[schedules.length - 1];
-  for (i = 0; i < currentSchedule.events.length; i++) {
-    if (currentSchedule.events[i].eventUUID == reqBody.eventUUID) {
-      currentSchedule.events[i].status = reqBody.status;
-      break;
-    }
-  }
-  currentSchedule.save();
-  return currentSchedule;
-}
-
-function getSchedule() {
-  const schedules = Schedule.find({});
-  const currentSchedule = schedules[schedules.length - 1];
-
-  return currentSchedule;
-}
-
-function changeScheduleStatus() {
+const changeScheduleStatus = async (req, res) => {
   const reqBody = { command: "hush" };
 
   if (!reqBody?.command) {
     return; //res.status(400);
   }
 
-  const schedules = Schedule.find({});
+  const schedules = await Schedule.find({});
   const currentSchedule = schedules[schedules.length - 1];
 
   switch (reqBody.command) {
@@ -134,6 +51,35 @@ function changeScheduleStatus() {
   currentSchedule.save();
 
   return currentSchedule;
-}
+};
+
+const changeEventStatus = async (req, res) => {
+  const reqBody = { eventUUID: "1", status: "busy" };
+
+  if (!(reqBody?.eventUUID && reqBody?.status)) {
+    return; // res.status(400);
+  }
+
+  const schedules = await Schedule.find({});
+  const currentSchedule = schedules[schedules.length - 1];
+  for (i = 0; i < currentSchedule.events.length; i++) {
+    if (currentSchedule.events[i].eventUUID == reqBody.eventUUID) {
+      currentSchedule.events[i].status = reqBody.status;
+      break;
+    }
+  }
+  currentSchedule.save();
+  return currentSchedule;
+};
+
+const getSchedule = async (req, res) => {
+  const schedules = await Schedule.find({});
+  const currentSchedule = schedules[schedules.length - 1];
+  return currentSchedule;
+};
+
+app.get("/schedule/get-current", (req, res) => getSchedule);
+app.post("/schedule/change-status", (req, res) => changeScheduleStatus);
+app.post("/event/change-status", (req, res) => changeEventStatus);
 
 main();

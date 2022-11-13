8b95944a0a3576ff1ebee4a7e548be3abb49a119
John Wilburn <johnwilburn2@gmail.com>
2022-11-12 18:55:10 -0600

    Try to run api

---
+++ App.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const mongoose = require("mongoose");
+const uri = "mongodb://127.0.0.1:27017/";
+
+const app = express();
+
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+async function main() {
+  mongoose.connect(uri);
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+  );
+}
+
+main().catch((err) => console.log(err));
